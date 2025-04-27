828562c4856b93649fb3f7cf8fafb50653eaa33e
bereket tesfaye <berekettesfaye7909@gmail.com>
2025-04-27 20:46:26 +0200

    added middlewares for parsing and  Cross-Origin Resource Sharing

--- server/src/express.js
+++ server/src/express.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 const app= express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 
 app.get('/',(req,res)=>{
     res.send("hello")
